ce5fa05fd37f58c35c62ece50de4d5a6caf38965
Jean Lange <jean.f.lange@gmail.com>
2021-10-29 11:59:29 -0400

    recursive add & print

--- main.rb
+++ main.rb
@@ -1,30 +1,38 @@
 class Node
     attr_accessor :next, :value
 
+    # this is a 'constructor'
     def initialize
         @value = nil
         @next = nil
     end
 
-    def add(new_value)
-        # step 1: if current value is empty, just add it here
-        # if the list is empty
-        current_value = @value
-        current_node = self
-        if current_value == nil
-            @value = new_value
-        # step 2: otherwise, if 'next' is empty, create a new node and add the value to that!
-        # if I'm at the end of the list
-        # step 3: otherwise, follow next and try again
-        # go to the end of the list and then do it...
+    def print
+        # returns a string
+        # print myself
+        # pass the message down
+        if @next
+            return @value + " -> " + @next.print
         else
-            # go to the end -
-            while current_node.next
-                current_node = current_node.next
+            return @value
+        end
+    end
+
+    def add(new_value)
+        # if the list is empty
+        if @value == nil
+            @value = new_value
+        # go to the end of the list and add a new node
+        else
+            # if I can, pass the message on
+            if @next
+                @next.add(new_value)
+            # create a new node with the value and tack it on
+            else
+                new_node = Node.new
+                new_node.value = new_value
+                @next = new_node
             end
-            new_node = Node.new
-            new_node.value = new_value
-            current_node.next = new_node
         end
     end
 end
@@ -45,11 +53,14 @@ head.add("jean")
 head.add("john")
 head.add("saturn")
 head.add("mars")
-
+head.add("gilly")
+head.add("new dog!")
+head.add("a clown!")
 
 # print it out!
 puts "Here's my conga line!"
-puts head.value + " -> "
-puts head.next.value + " -> "
-puts head.next.next.value + " -> "
-puts head.next.next.next.value + " -> "
+puts head.print
+# puts head.value + " -> "
+# puts head.next.value + " -> "
+# puts head.next.next.value + " -> "
+# puts head.next.next.next.value + " -> "
